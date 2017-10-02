c46b5f0d825dfc78737c3deb9bc9ded440c46bc4
Macintosh <macintosh@Macintoshs-iMac.local>
2017-10-02 17:49:46 +0300

    Initial Commit

---
+++ Affter Editor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Affter Editor
+//
+//  Created by Macintosh on 02.10.17.
+//  Copyright © 2017 Red Owl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
